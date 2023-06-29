74cc62615b258bb9382edac00f0f0b621a3b2b94
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-06-29 16:51:54 +0100

    Update 2.6.1.9.py

--- 2.6.1.9.py
+++ 2.6.1.9.py
@@ -1,4 +1,4 @@
-﻿# input a float value for variable a here
+# input a float value for variable a here
 var_a = float(input("enter variable a here: "))
 # input a float value for variable b here
 var_b = float(input("enter variable b here: "))
@@ -15,8 +15,16 @@ multiplication = (var_a * var_b)
 # output the result of division here
 division = (var_a / var_b)
 
+# output the result of expo here
+expo = var_a ** var_b
+
+# output the result of floor-division here
+floor_div = var_a // var_b
+
 print("addition result: ", addition)
 print("subtraction result: ", subtraction)
 print("multiplication result: ", multiplication)
 print("division result: ", division)
+print("expo result: ", expo)
+print("floor_division result: ", floor_div)
 print("\nThat's all, folks!")
