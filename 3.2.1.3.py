a1896c21de17f927804c13808c2542077540a352
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-07-03 11:21:48 +0100

    Add files via upload

---
+++ 3.2.1.3.py
@@ -0,0 +1,18 @@
+﻿secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+number = int(input("enter the number: "))
+while number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    number = int(input("enter the number: "))
+else:
+    print("Well done, muggle! You are free now.")
