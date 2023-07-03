97daa64241340e1145acef9cf2352a9165b25e81
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-07-03 08:58:02 +0100

    Add files via upload

---
+++ 3.1.1.10.py
@@ -0,0 +1,12 @@
+﻿word1 = input("Enter the first number: ")
+word2 = "Spathiphyllum"
+word3 = "spathiphyllum"
+
+if word1 == word2:
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif word1 == word3:
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not", word1, "!" )
+
+############################################
---
+++ 3.1.1.4.py
@@ -0,0 +1,8 @@
+﻿n = int(input()) # enter a number
+
+if n >= 100:        # if n is greater than and equal to 100
+    print("True")   # If n is greater than or equal to 100, output is True
+else:               # otherwise
+    print("False")  # output is False
+
+#####################
