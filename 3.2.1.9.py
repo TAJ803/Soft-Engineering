f321a94ff073d18e0f72b7fa9801fc252ed19305
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-07-04 12:47:17 +0100

    Add files via upload

---
+++ 3.2.1.9.py
@@ -0,0 +1,7 @@
+﻿word = input("enter a word :")
+while word != "chupacabra":
+    word = input("enter a word :")
+    
+    if word == "chupacabra":
+        break
+print("You've successfully left the loop.")
