6e9e28f1a68e8ea6521dff93e69d82fdb97b2463
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-07-04 12:48:09 +0100

    Add files via upload

---
+++ 3.2.1.10.py
@@ -0,0 +1,26 @@
+﻿# Prompt the user to enter a word
+# and assign it to the user_word variable.
+
+user_word = input("enter a word :")
+
+user_word = user_word.upper()
+#print(user_word)
+
+letter = "A", "E", "I", "O", "U" 
+
+for letter in user_word:
+    # Complete the body of the for loop.
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
+#print(user_word)
+        
