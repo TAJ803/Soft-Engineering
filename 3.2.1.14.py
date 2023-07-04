3cf01373c5ed9859e580ac926ffed278e8d9c7d4
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-07-04 13:07:05 +0100

    Add files via upload

---
+++ 3.2.1.14.py
@@ -0,0 +1,16 @@
+﻿blocks = int(input("Enter the number of blocks: "))
+
+height = 0
+blocks = 0
+layer_number = 0
+
+while blocks > 0:
+
+    if blocks - layer_number < 0:
+        break
+    else:
+        layer_number = layer_number + 1
+        height = height + 1
+        blocks = blocks - layer_number
+
+print("The height of the pyramid:", height)
