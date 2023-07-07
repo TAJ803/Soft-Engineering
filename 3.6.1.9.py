474fb806f97a832359d96c2c4bb02f2990314366
TAJ803 <60465024+TAJ803@users.noreply.github.com>
2023-07-07 09:13:39 +0100

    Add files via upload

---
+++ 3.6.1.9.py
@@ -0,0 +1,11 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+#
+# Write your code here.
+#
+my_list_2 = []
+
+for i in my_list:
+    if i not in my_list_2:
+        my_list_2.append(i)
+
+print(my_list_2)
